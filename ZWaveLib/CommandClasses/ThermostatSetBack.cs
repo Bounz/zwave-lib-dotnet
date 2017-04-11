c28efeb0ea9dd8842bde141f8c390cb0a2855efa
Alexander Sidorenko <sidorlutiy@gmail.com>
2017-04-11 19:07:56 +0300

    Tests for ThermostatSetback command class

--- ZWaveLib/CommandClasses/ThermostatSetBack.cs
+++ ZWaveLib/CommandClasses/ThermostatSetBack.cs
@@ -20,8 +20,15 @@
  *     Project Homepage: https://github.com/genielabs/zwave-lib-dotnet
  */
 
+using System;
+
 namespace ZWaveLib.CommandClasses
 {
+    /// <summary>
+    /// The Thermostat Setback Command Class is used to change the current state of a non-schedule setback
+    /// thermostat
+    /// </summary>
+    /// <remarks>SDS12652 3.43 Thermostat Setback Command Class, version 1</remarks>
     public class ThermostatSetBack : ICommandClass
     {
         public CommandClass GetClassId()
@@ -29,9 +36,32 @@ namespace ZWaveLib.CommandClasses
             return CommandClass.ThermostatSetBack;
         }
 
+        // SDS12652 3.43.3 Thermostat Setback Report Command
         public NodeEvent GetEvent(IZWaveNode node, byte[] message)
         {
             return new NodeEvent(node, EventParameter.ThermostatSetBack, message[2], 0);
+        }
+
+        /// <summary>
+        /// The Thermostat Setback Set Command is used to set the state of the thermostat.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// <remarks>SDS12652 3.43.1 Thermostat Setback Set Command</remarks>
+        public static ZWaveMessage Set(IZWaveNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The Thermostat Setback Get Command is used to request the current state of the thermostat
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// <remarks>SDS12652 3.43.2 Thermostat Setback Get Command</remarks>
+        public static ZWaveMessage Get(IZWaveNode node)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ ZWaveLibTests/CommandClasses/ThermostatSetbackTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using NUnit.Framework;
+using ZWaveLib;
+using ZWaveLib.CommandClasses;
+
+namespace ZWaveLibTests.CommandClasses
+{
+    [TestFixture]
+    public class ThermostatSetbackTests
+    {
+        /*
+         * Information about the constants is taken from
+         * "Sigma Designs, SDS13740, Software Design Specification, Z-Wave Device and Command Class Types and Defines Specification"
+         * page 107, Thermostat Setback command class commands
+         */
+
+        private const byte CommandClassThermostatSetback = 0x47;   // COMMAND_CLASS_THERMOSTAT_SETBACK
+        private const byte ThermostatSetbackVersion = 0x01;        // THERMOSTAT_SETBACK_VERSION
+        private const byte ThermostatSetbackGet = 0x02;            // THERMOSTAT_SETBACK_GET
+        private const byte ThermostatSetbackReport = 0x03;         // THERMOSTAT_SETBACK_REPORT
+        private const byte ThermostatSetbackSet = 0x01;            // THERMOSTAT_SETBACK_SET
+
+
+        [Test]
+        public void GetCommandClass()
+        {
+            var commandClass = new ThermostatSetBack();
+            var classId = (byte) commandClass.GetClassId();
+
+            Assert.That(classId, Is.EqualTo(CommandClassThermostatSetback));
+        }
+
+        [Test]
+        public void GetEvent_Parses_ReportCommand()
+        {
+            var commandClass = new ThermostatSetBack();
+            var message = new byte[] { CommandClassThermostatSetback, ThermostatSetbackReport, 0x01, 0x02 };
+
+            var nodeEvent = commandClass.GetEvent(new ZWaveNode(), message);
+
+            Assert.That(nodeEvent, Is.Not.Null);
+            Assert.That(nodeEvent.Parameter, Is.EqualTo(EventParameter.ThermostatSetBack));
+            Assert.That(nodeEvent.Value, Is.EqualTo(0x01));
+        }
+
+        [Test]
+        public void SetMessage()
+        {
+            var node = new Mock<IZWaveNode>();
+
+            Assert.That(() => ThermostatSetBack.Set(node.Object), Throws.Exception);
+
+        }
+
+        [Test]
+        public void GetMessage()
+        {
+            var node = new Mock<IZWaveNode>();
+
+            Assert.That(() => ThermostatSetBack.Set(node.Object), Throws.Exception);
+        }
+    }
+}
